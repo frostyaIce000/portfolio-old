61c7516de66c2b48ba3abf3f4c6609d1abe8351c
frostyalce000 <frosty.alce000@gmail.com>
2025-09-09 10:37:17 +0200

    Fix gitlab-ci

--- frontend-angular/karma.conf.js
+++ frontend-angular/karma.conf.js
@@ -1,7 +1,36 @@
 module.exports = function (config) {
   config.set({
-    // ... autres configs
-    browsers: ['Chrome', 'ChromeHeadless'],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution order
+        random: true
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/angular-starter'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
@@ -9,9 +38,12 @@ module.exports = function (config) {
           '--headless',
           '--disable-gpu',
           '--no-sandbox',
-          '--disable-dev-shm-usage'
+          '--disable-dev-shm-usage',
+          '--disable-web-security',
+          '--remote-debugging-port=9222'
         ]
       }
-    }
+    },
+    restartOnFileChange: true
   });
 };
