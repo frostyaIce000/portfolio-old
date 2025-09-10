d4322259ed67ca6f24e1c2788a0bdac828d7ea01
frostyalce000 <frosty.alce000@gmail.com>
2025-09-10 10:47:39 +0200

    Improve behavior-subject on examples-angular

--- examples-angular/apps/angular-behavior-subject/src/app/app.ts
+++ examples-angular/apps/angular-behavior-subject/src/app/app.ts
@@ -1,91 +1,58 @@
-import { Component } from '@angular/core'
-import { AsyncPipe, CommonModule } from '@angular/common'
-import { BehaviorSubject, map, Observable } from 'rxjs'
+import { Component, OnDestroy } from '@angular/core'
+import { CommonModule, AsyncPipe } from '@angular/common'
+import { BehaviorSubject, Subscription } from 'rxjs'
+import { CounterComponent } from './counter'
+import { LabelComponent } from './label'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, AsyncPipe],
-  templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  imports: [CommonModule, AsyncPipe, CounterComponent, LabelComponent],
+  templateUrl: './app.html'
 })
-export class App {
-  private readonly countSubject = new BehaviorSubject<number>(0)
+export class App implements OnDestroy {
+  counter = new BehaviorSubject<number>(0)
+  counter$ = this.counter.asObservable()
+  private sub: Subscription
 
-  readonly count$: Observable<number> = this.countSubject.asObservable()
-  readonly double$: Observable<number> = this.count$.pipe(map(v => v * 2))
-
-  inc(): void {
-    this.countSubject.next(this.countSubject.value + 10)
+  constructor() {
+    this.sub = this.counter$.subscribe(v => console.log('Subscriber A:', v))
   }
 
-  dec(): void {
-    this.countSubject.next(this.countSubject.value - 10)
+  inc() {
+    this.counter.next(this.counter.value + 1)
   }
 
-  reset(): void {
-    this.countSubject.next(0)
+  dec() {
+    this.counter.next(this.counter.value - 1)
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe()
   }
 }
 
 
 
-// import { Component } from '@angular/core'
-// import { BehaviorSubject, map, Observable } from 'rxjs'
+// import { Component, OnDestroy } from '@angular/core'
+// import { CommonModule, AsyncPipe } from '@angular/common'
+// import { BehaviorSubject, Subscription } from 'rxjs'
+// import { CounterComponent } from './counter'
+// import { LabelComponent } from './label'
 
 // @Component({
 //   selector: 'app-root',
-//   templateUrl: './app.html',
-//   styleUrls: ['./app.css']
+//   standalone: true,
+//   imports: [CommonModule, AsyncPipe, CounterComponent, LabelComponent],
+//   templateUrl: './app.html'
 // })
-// export class App {
-//   private readonly countSubject = new BehaviorSubject<number>(0)
+// export class App implements OnDestroy {
+//   counter = new BehaviorSubject<number>(0)
+//   counter$ = this.counter.asObservable()
+//   private sub: Subscription = this.counter$.subscribe(v => console.log('Subscriber A:', v))
 
-//   readonly count$: Observable<number> = this.countSubject.asObservable()
-//   readonly double$: Observable<number> = this.count$.pipe(map(v => v * 2))
+//   inc() { this.counter.next(this.counter.value + 1) }
+//   dec() { this.counter.next(this.counter.value - 1) }
 
-//   inc(): void {
-//     this.countSubject.next(this.countSubject.value + 10)
-//   }
-
-//   dec(): void {
-//     this.countSubject.next(this.countSubject.value - 10)
-//   }
-
-//   reset(): void {
-//     this.countSubject.next(0)
-//   }
+//   ngOnDestroy() { this.sub.unsubscribe() }
 // }
-
-
-
-// import { Component } from '@angular/core';
-// import { BehaviorSubject, map } from 'rxjs';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.html',
-//   styleUrls: ['./app.css']
-// })
-// export class AppComponent {
-//   count$ = new BehaviorSubject<number>(0);
-//   double$ = this.count$.pipe(map(v => v * 2));
-
-//   inc() {
-//     this.count$.next(this.count$.value + 10);
-//   }
-
-//   dec() {
-//     this.count$.next(this.count$.value - 10);
-//   }
-
-//   reset() {
-//     this.count$.next(0);
-//   }
-
-//   constructor() {
-//     console.log('00000000001:', this.count$);
-//     this.double$.subscribe(); 
-//   }
-// }
-
